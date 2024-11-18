904d32d760d7bc5cbe21ffc33110ffef6a696db8
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-11-18 13:10:06 +0900

    :memo: BearerTokenGuard 구현

---
+++ src/auth/guard/bearer-token.guard.ts
@@ -0,0 +1,71 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { UsersService } from '../../users/users.service';
+
+@Injectable()
+export abstract class BearerTokenGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    const rawToken = req.headers['authorization'];
+
+    if (!rawToken) {
+      throw new UnauthorizedException('not found token');
+    }
+
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const result = this.authService.verifyToken(token);
+
+    /*
+     * 1) 사용자 정보 - user
+     * 2) token - token
+     * 3) tokenType - access | refresh
+     * */
+    req.user = await this.usersService.getUserByEmail(result.email);
+    req.token = token;
+    req.tokenType = result.type;
+
+    return true;
+  }
+}
+
+@Injectable()
+export class AccessTokenGuard extends BearerTokenGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+
+    const req = context.switchToHttp().getRequest();
+
+    if (req.tokenType !== 'access') {
+      throw new UnauthorizedException('not found access token');
+    }
+
+    return true;
+  }
+}
+
+@Injectable()
+export class RefreshTokenGuard extends BearerTokenGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    await super.canActivate(context);
+
+    const req = context.switchToHttp().getRequest();
+
+    if (req.tokenType !== 'refresh') {
+      throw new UnauthorizedException('not found refresh token');
+    }
+
+    return true;
+  }
+}
