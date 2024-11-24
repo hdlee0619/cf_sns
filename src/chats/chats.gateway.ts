b0d4c1e947ceadacb79aba7b4a3c934b39e9f164
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-11-24 15:01:58 +0900

    :memo: WebSocket 심화
    - 웹소켓 인증
    - life cycle

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -2,6 +2,8 @@ import {
   ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -15,21 +17,58 @@ import { CreateMessageDto } from './messages/dto/create-message.dto';
 import { ChatsMessagesService } from './messages/messages.service';
 import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { SocketCatchHttpExceptionFilter } from '../common/exception/socket-catch-http.exception-filter';
+import { UsersModel } from '../users/entities/users.entity';
+import { UsersService } from '../users/users.service';
+import { AuthService } from '../auth/auth.service';
 
 @WebSocketGateway({
   namespace: 'chats',
 })
-export class ChatsGateway implements OnGatewayConnection {
+export class ChatsGateway
+  implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect
+{
   constructor(
     private readonly chatsService: ChatsService,
     private readonly messagesService: ChatsMessagesService,
+    private readonly userService: UsersService,
+    private readonly authService: AuthService,
   ) {}
 
   @WebSocketServer()
   server: Server;
 
-  handleConnection(socket: Socket) {
+  // WebSocket이 연결 되었을 때 실행
+  async handleConnection(socket: Socket & { user: UsersModel }) {
     console.log(`on connect called : ${socket.id}`);
+
+    try {
+      const headers = socket.handshake.headers;
+
+      const rawToken = headers['authorization'];
+
+      if (!rawToken) {
+        socket.disconnect();
+      }
+
+      const token = this.authService.extractTokenFromHeader(rawToken, true);
+      const payload = this.authService.verifyToken(token);
+
+      socket.user = await this.userService.getUserByEmail(payload.email);
+
+      return true;
+    } catch (e) {
+      socket.disconnect();
+    }
+  }
+
+  // Gateway가 초기화 되었을 때 실행
+  afterInit(server: any): any {
+    console.log('after gateway init');
+  }
+
+  // WebSocket이 연결이 끊겼을 때 실행
+  handleDisconnect(socket: any): any {
+    console.log(`on disconnect called : ${socket.id}`);
   }
 
   @UsePipes(
@@ -46,16 +85,27 @@ export class ChatsGateway implements OnGatewayConnection {
   @SubscribeMessage('create_chat')
   async createChat(
     @MessageBody() data: CreateChatDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     const chat = await this.chatsService.createChat(data);
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
   @SubscribeMessage('enter_chat')
   async enterChat(
     // 방의 ID를 리스트로
     @MessageBody() data: EnterChatDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     for (const chatId of data.chatIds) {
       const exists = await this.chatsService.checkIfChatExists(chatId);
@@ -70,10 +120,21 @@ export class ChatsGateway implements OnGatewayConnection {
     socket.join(data.chatIds.map((x) => x.toString()));
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+  @UseFilters(SocketCatchHttpExceptionFilter)
   @SubscribeMessage('send_message')
   async sendMessage(
     @MessageBody() dto: CreateMessageDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     const chatExists = await this.chatsService.checkIfChatExists(dto.chatId);
 
@@ -83,7 +144,10 @@ export class ChatsGateway implements OnGatewayConnection {
       });
     }
 
-    const message = await this.messagesService.createMessage(dto);
+    const message = await this.messagesService.createMessage(
+      dto,
+      socket.user.id,
+    );
 
     socket.to(message.chat.id.toString()).emit('receive_message', dto);
   }
